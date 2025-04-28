72e4922758f2d27454a62fed613953cc0a93ac4c
Coderex._.bob <140780835+EricbobXD@users.noreply.github.com>
2025-04-28 08:59:18 +0800

    Update category.text.js

--- public/toolbox/category.text.js
+++ public/toolbox/category.text.js
@@ -40,6 +40,113 @@ Blockly.Blocks['add_line'] = {
   }
 };
 
+// 定義方塊：string_generic
+Blockly.Blocks['string_generic'] = {
+    init: function() {
+        this.setColour("#71b700");
+        this.setOutput(true, "String");
+        this.setInputsInline(true);
+        this.setMutator(new Blockly.Mutator(['string_generic_item']));
+        this.itemCount_ = 2; // 預設至少兩個輸入欄位
+        this.operator_ = '+'; // 預設運算符為加法
+        this.updateShape_();
+    },
+    mutationToDom: function() {
+        const container = document.createElement('mutation');
+        container.setAttribute('items', this.itemCount_);
+        container.setAttribute('operator', this.operator_);
+        return container;
+    },
+    domToMutation: function(xmlElement) {
+        this.itemCount_ = Math.max(2, parseInt(xmlElement.getAttribute('items'), 10)); // 確保最少兩個
+        this.operator_ = xmlElement.getAttribute('operator') || '+';
+        this.updateShape_();
+    },
+    decompose: function(workspace) {
+        const containerBlock = workspace.newBlock('string_generic_container');
+        containerBlock.initSvg();
+        let connection = containerBlock.getInput('STACK').connection;
+        for (let i = 0; i < this.itemCount_; i++) {
+            const itemBlock = workspace.newBlock('string_generic_item');
+            itemBlock.initSvg();
+            connection.connect(itemBlock.previousConnection);
+            connection = itemBlock.nextConnection;
+        }
+        return containerBlock;
+    },
+    compose: function(containerBlock) {
+        let itemBlock = containerBlock.getInputTargetBlock('STACK');
+        const connections = [];
+        while (itemBlock) {
+            connections.push(itemBlock.valueConnection_);
+            itemBlock = itemBlock.nextConnection && itemBlock.nextConnection.targetBlock();
+        }
+        this.itemCount_ = Math.max(2, connections.length); // 確保最少兩個
+        this.updateShape_();
+        for (let i = 0; i < this.itemCount_; i++) {
+            Blockly.Mutator.reconnect(connections[i], this, 'ADD' + i);
+        }
+    },
+    updateShape_: function() {
+        // 移除多餘的輸入
+        let i = 0;
+        while (this.getInput('ADD' + i)) {
+            this.removeInput('ADD' + i);
+            i++;
+        }
+
+        // 添加所需的輸入
+        for (let j = 0; j < this.itemCount_; j++) {
+            const input = this.appendValueInput('ADD' + j).setCheck("String");
+            if (j > 0) {
+                input.appendField(this.operator_);
+            }
+        }
+    },
+    setOperator: function(operator) {
+        this.operator_ = operator;
+        this.updateShape_();
+    }
+};
+
+// 定義方塊：string_generic_container
+Blockly.Blocks['string_generic_container'] = {
+    init: function() {
+        this.setColour("#71b700");
+        this.appendDummyInput().appendField("輸入");
+        this.appendStatementInput('STACK');
+        this.contextMenu = false;
+    }
+};
+
+// 定義方塊：string_generic_item
+Blockly.Blocks['string_generic_item'] = {
+    init: function() {
+        this.setColour("#71b700");
+        this.appendDummyInput().appendField("項目");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.contextMenu = false;
+    }
+};
+
+function defineStringOperatorBlock(type, operatorSymbol) {
+    Blockly.Blocks[type] = Object.assign({}, Blockly.Blocks['string_generic'], {
+        init: function() {
+            Blockly.Blocks['string_generic'].init.call(this);
+            this.setOperator(operatorSymbol);
+        }
+    });
+}
+
+//string
+defineStringOperatorBlock('string_plus', '+');
+defineStringOperatorBlock('string_commas', ',');
+defineStringOperatorBlock('string_cin', '>>');
+defineStringOperatorBlock('string_cout', '<<');
+
+Blockly.Cpp = Blockly.Cpp || {};
+
 // C++ 語法生成器：add_line
 Blockly.Cpp['add_line'] = function(block) {
   // 產生換行符號
@@ -62,4 +169,24 @@ Blockly.Cpp['char'] = function(block) {
 Blockly.Cpp['comment_block'] = function(block) {
   var comment = block.getFieldValue('COMMENT') || '';
   return '// ' + comment + '\n';
+};
+
+// C++ 語法生成器：string_plus
+Blockly.Cpp['string_plus'] = function(block) {
+    return string_generateCode(block, ' + ');
+};
+
+// C++ 語法生成器：string_commas
+Blockly.Cpp['string_commas'] = function(block) {
+    return string_generateCode(block, ' , ');
+};
+
+// C++ 語法生成器：string_cout
+Blockly.Cpp['string_cout'] = function(block) {
+    return string_generateCode(block, ' << ');
+};
+
+// C++ 語法生成器：string_cin
+Blockly.Cpp['string_cin'] = function(block) {
+    return string_generateCode(block, ' >> ');
 };
